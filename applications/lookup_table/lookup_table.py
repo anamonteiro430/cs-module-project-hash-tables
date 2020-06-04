7539a0f7fd5e802a71ce91b231f712f527447943
kika <anamonteiro430@gmail.com>
2020-06-04 12:05:30 +0100

    lookup  table

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,5 +1,7 @@
-# Your code here
+import random
+import math
 
+numbers = {}
 
 def slowfun_too_slow(x, y):
     v = math.pow(x, y)
@@ -14,8 +16,11 @@ def slowfun(x, y):
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
-    # Your code here
-
+    
+    if (x,y) not in numbers:
+        numbers[(x,y)] = slowfun_too_slow(x, y)
+        
+    return numbers[(x,y)]
 
 
 # Do not modify below this line!
@@ -23,4 +28,6 @@ def slowfun(x, y):
 for i in range(50000):
     x = random.randrange(2, 14)
     y = random.randrange(3, 6)
-    print(f'{i}: {x},{y}: {slowfun(x, y)}')
+    print(f'{i}: {x},{y}: {slowfun(x, y)}') 
+
+
